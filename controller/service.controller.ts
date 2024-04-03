caac6fa7eeaa4e53e7cd5d868cf0f007ef641c6b
Kashish <kashish.biserwal@protonmail.com>
2024-04-03 16:26:37 +0530

    service update

--- controller/service.controller.ts
+++ controller/service.controller.ts
@@ -24,7 +24,27 @@ export const CreateService = async (req: ExtendedRequest, res: Response, next: N
     return res.status(200).send({ status: 201, message: 'Created', service: service })
 }
 
-export const GetServices = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+export const GetAllServices = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    const query = req.query
+    const { page = 1, limit = 10 } = query
+    if (isNaN(Number(page)) || isNaN(Number(limit))) {
+        return res
+            .status(200)
+            .send({ status: 400, error: 'Bad Request', error_description: 'Invalid Query Parameters' })
+    }
+    const skip = (Number(page) - 1) * Number(limit)
+    try {
+        const services = await prisma.service.findMany({
+            skip: skip,
+            take: Number(limit)
+        })
+        return res.status(200).send({ status: 200, message: 'Ok', services: services })
+    } catch (err) {
+        return next(err)
+    }
+}
+
+export const GetServicesByPincode = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
     const query = req.query
     const { page = 1, limit = 10, pincode } = query
     if (isNaN(Number(page)) || isNaN(Number(limit)) || isNaN(Number(pincode))) {
@@ -68,5 +88,23 @@ export const getSpecificService = async (req: ExtendedRequest, res: Response, ne
     return res.status(200).send({ status: 200, message: 'Ok', service })
 }
 
-const serviceController = { CreateService, GetServices, getSpecificService }
+export const deleteService = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    let serviceId: string | number = req.params.id
+    if (!serviceId) {
+        return res
+            .status(200)
+            .send({ status: 400, error: 'Invalid payload', error_description: 'id(service) is required in params.' })
+    }
+    serviceId = Number(serviceId)
+    if (Number.isNaN(serviceId)) {
+        return res
+            .status(200)
+            .send({ status: 400, error: 'Invalid payload', error_description: 'id(service) should be a number.' })
+    }
+
+    const service = await prisma.service.delete({ where: { id: serviceId }})
+    return res.status(200).send({ status: 200, message: 'Deleted', service })
+}
+
+const serviceController = { CreateService, GetAllServices, GetServicesByPincode, getSpecificService, deleteService }
 export default serviceController
--- routes/service.routes.ts
+++ routes/service.routes.ts
@@ -7,9 +7,13 @@ ServiceRouter
     //@ts-ignore
     .post('/', serviceController.CreateService)
     //@ts-ignore
-    .get('/', serviceController.GetServices)
+    .get('/all', serviceController.GetAllServices)
+    //@ts-ignore
+    .get('/', serviceController.GetServicesByPincode)
     //@ts-ignore
     .get('/:id', serviceController.getSpecificService)
+    //@ts-ignore
+    .delete('/:id', serviceController.deleteService)
 
     
 export default ServiceRouter

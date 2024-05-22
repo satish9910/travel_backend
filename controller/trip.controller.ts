e7b6a2b4887d0d7ed9aa52ea742afd56edf60bd8
Kashish <kashish.biserwal@protonmail.com>
2024-05-22 11:31:16 +0530

    helper change

--- controller/trip.controller.ts
+++ controller/trip.controller.ts
@@ -33,6 +33,15 @@ export const CreateTrip = async (req: ExtendedRequest, res: Response, next: Next
             host_id: service.host_id,
         },
     })
+    if (service.type === 1) {
+        if (service.available_seats !== null && service.available_seats < body.number_of_people) {
+            return res.status(200).send({ status: 400, error: 'Not enough seats', error_description: 'Service does not have enough seats.' })
+        }
+        await prisma.service.update({
+            where: { id: body.service_id },
+            data: { available_seats: service.available_seats !== null ? service.available_seats - body.number_of_people : null },
+        })
+    }
     return res.status(200).send({ status: 201, message: 'Created', trip: trip })
 }
 

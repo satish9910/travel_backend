d26236bc4136c753e74934bc05771ae3450d3ad5
Kashish <kashish.biserwal@protonmail.com>
2024-05-31 16:08:22 +0530

    posts cloud, get_only_videos updated

--- controller/posts.controller.ts
+++ controller/posts.controller.ts
@@ -7,14 +7,14 @@ const prisma = new PrismaClient()
 export const CreatePost = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
     const user = req.user
     const body = req.body
-    if (!helper.isValidatePaylod(body, ['description']) || !req.file) {
+    if (!helper.isValidatePaylod(body, ['image', 'description'])) {
         return res
             .status(200)
             .send({ status: 200, error: 'Invalid payload', error_description: 'description & image is required.' })
     }
     const post = await prisma.post.create({
         data: {
-            image: helper.imageUrlGen(req.file.filename),
+            image: body.image,
             description: body.description,
             user_id: user.id,
             media_type: body.media_type,
@@ -25,13 +25,73 @@ export const CreatePost = async (req: ExtendedRequest, res: Response, next: Next
     })
     return res.status(200).send({ status: 201, message: 'Created', post: post })
 }
+// export const CreatePost = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+//     const user = req.user
+//     const body = req.body
+//     if (!helper.isValidatePaylod(body, ['description']) || !req.file) {
+//         return res
+//             .status(200)
+//             .send({ status: 200, error: 'Invalid payload', error_description: 'description & image is required.' })
+//     }
+//     const post = await prisma.post.create({
+//         data: {
+//             image: helper.imageUrlGen(req.file.filename),
+//             description: body.description,
+//             user_id: user.id,
+//             media_type: body.media_type,
+//             latitude: body.latitude,
+//             longitude: body.longitude,
+//             place: body.place,
+//         },
+//     })
+//     return res.status(200).send({ status: 201, message: 'Created', post: post })
+// }
 
 export const GetOnlyVideos = async (req: ExtendedRequest, res: Response, _next: NextFunction) => {
-    const user = req.user
-    const videos = await prisma.post.findMany({ where: { user_id: user.id, media_type: 'VIDEO' } })
+    const query = req.query
+    const { page = 1, limit = 20 } = query
+    if (Number.isNaN(page) || Number.isNaN(limit))
+        return res.status(200).send({
+            status: 400,
+            error: 'Invalid query parameters',
+            error_description: 'skip, limit should be a number',
+        })
 
-    return res.status(200).send({ status: 200, message: 'Ok', videos })
+    const skip = (Number(page) - 1) * Number(limit)
+    try {
+        const fetchPosts = await prisma.post.findMany({
+            where: { media_type: 'VIDEO' },
+            include: {
+            user: {
+                select: {
+                id: true,
+                username: true,
+                image: true,
+                },
+            },
+            comment: true
+            },
+            orderBy: { created_at: 'desc' },
+        })
+        for (let i = 0; i < fetchPosts.length; i++) {
+            const isLiked = await prisma.likes.findFirst({
+                where: { post_id: fetchPosts[i].id, user_id: req.user.id },
+            })
+            //@ts-ignore
+            fetchPosts[i].isLiked = isLiked ? true : false
+        }
+        return res.status(200).send({ status: 200, message: 'Ok', posts: fetchPosts })
+    } catch (err) {
+        return _next(err)
+    }
 }
+
+// export const GetOnlyVideos = async (req: ExtendedRequest, res: Response, _next: NextFunction) => {
+//     const user = req.user
+//     const videos = await prisma.post.findMany({ where: { user_id: user.id, media_type: 'VIDEO' } })
+
+//     return res.status(200).send({ status: 200, message: 'Ok', videos })
+// }
 
 export const GetPosts = async (req: ExtendedRequest, res: Response, _next: NextFunction) => {
     const user = req.user
--- routes/post.routes.ts
+++ routes/post.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import postController from '../controller/posts.controller'
-import { upload } from '..'
-import middleware from '../utils/middleware'
+// import { upload } from '..'
+// import middleware from '../utils/middleware'
 const postRouter = Router()
 
 //@ts-ignore
@@ -13,7 +13,8 @@ postRouter
     //@ts-ignore
     .get('/:id', postController.GetSpecificPost)
     //@ts-ignore
-    .post('/', upload.single('image'), postController.CreatePost)
+    .post('/', postController.CreatePost)
+    // .post('/', upload.single('image'), postController.CreatePost)
     //@ts-ignore
     .delete('/:id', postController.DeletePost)
     //@ts-ignore

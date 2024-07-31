35a9a55eb240be2a71e4782accb265e599e0841e
Kashish <kashish.biserwal@protonmail.com>
2024-07-31 17:57:01 +0530

    phone otp send, verify

--- controller/auth.controller.ts
+++ controller/auth.controller.ts
@@ -140,7 +140,7 @@ const Signup = async (req: Request, res: Response, next: NextFunction) => {
 
 const SendOtp = async (req: Request, res: Response, _next: NextFunction) => {
     if (!helper.isValidatePaylod(req.body, ['email'])) {
-        return res.status(200).send({ status: 400, error: 'Invalid Payload', error_description: 'phone requried' })
+        return res.status(200).send({ status: 400, error: 'Invalid Payload', error_description: 'Email requried' })
     }
     const { email } = req.body
     const otp = Math.floor(10000 + Math.random() * 90000)
@@ -375,5 +375,81 @@ const superAdminLogin = async (req: Request, res: Response, next: NextFunction) 
     })
 }
 
-const authController = { Login, ForgotPassword, Signup, SendOtp, VerifyOtp, HostLogin, socialLogin, superAdminLogin }
+const SendOtpPhone = async (req: Request, res: Response, _next: NextFunction) => {
+    if (!helper.isValidatePaylod(req.body, ['phone'])) {
+        return res.status(200).send({ status: 400, error: 'Invalid Payload', error_description: 'phone requried' })
+    }
+    const { phone } = req.body
+    // const otp = Math.floor(10000 + Math.random() * 90000)
+    const otp = 1234
+    const user = await prisma.user.findFirst({ where: { phone } })
+    if (!user) return res.status(200).send({ status: 404, error: 'Not found', error_description: 'user not found' })
+    const previousSendOtp = await prisma.otp.findUnique({ where: { user_id: user.id } })
+    const userid = user.id
+    if (!previousSendOtp) {
+        try {
+            const otpData = await prisma.otp.create({ data: { user_id: userid, otp: otp } })
+            // helper.sendMail(phone , 'TravelApp Acco-unt Verification', `Your OTP is ${otp}`)
+        } catch (err) {
+            return _next(err)
+        }
+        return res.status(200).send({ status: 200, message: 'Ok' })
+    } else {
+        try {
+            const otpData = await prisma.otp.update({ where: { user_id: userid }, data: { otp: otp } })
+            // helper.sendMail(phone , 'TravelApp Account Verification', `Your OTP is ${otp}`)
+        } catch (err) {
+            return _next(err)
+        }
+        return res.status(200).send({ status: 200, message: 'Ok' })
+    }
+}
+
+/**
+ * controller to verify otp from Otp using Otp and email table
+ * Every otp will exipred after 5 minute
+ * @param req
+ * @param res
+ * @param next
+ * @returns responses
+ */
+const VerifyOtpPhone = async (req: Request, res: Response, next: NextFunction) => {
+    const { phone, otp } = req.body
+    if (!helper.isValidatePaylod(req.body, ['phone', 'otp'])) {
+        return res
+            .status(200)
+            .send({ status: 400, error: 'Invalid payload', error_description: 'phone, otp are required.' })
+    }
+    const user = await prisma.user.findFirst({ where: { phone } })
+    if (user?.is_verified) {
+        return res.status(200).send({ status: 400, error: 'Bad Request', error_description: 'User already verified' })
+    }
+    if (!user)
+        return res
+            .status(200)
+            .send({ status: 400, error: 'user not found.', error_description: `No user with ${phone}` })
+    const otpData = await prisma.otp.findUnique({ where: { user_id: user.id } })
+    if (!otpData) {
+        return res.status(200).send({ error: 'Bad Request', error_description: 'OTP is not valid.' })
+    }
+    if (otpData?.otp === otp) {
+        const otpExpirationTime = new Date(otpData.updated_at).setMinutes(new Date().getMinutes() + 5)
+        if (otpExpirationTime < new Date().getTime()) {
+            return res.status(200).send({ status: 400, error: 'Bad Request', error_description: 'OTP is expired.' })
+        }
+        try {
+            const updatedUser = await prisma.user.update({ where: { id: user.id }, data: { is_verified: true } })
+            const token = jwt.sign({ phone: user.phone }, process.env.JWT_SECRET!, {
+                expiresIn: '7d',
+            })
+            return res.status(200).send({ status: 200, message: 'Ok', user: updatedUser, token })
+        } catch (err) {
+            return next(err)
+        }
+    } else {
+        return res.status(200).send({ status: 400, error: 'Bad Request', error_description: 'OTP is not valid.' })
+    }
+}
+
+const authController = { Login, ForgotPassword, Signup, SendOtp, VerifyOtp, HostLogin, socialLogin, superAdminLogin, SendOtpPhone, VerifyOtpPhone }
 export default authController
